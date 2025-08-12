029d573c7ade2287287af92cc1aa7284ec8e515b
Akshay Gandhi <akshay@zurumedia.com>
2025-08-12 17:36:35 +0530

    404 and other pages are created

--- resources/views/front/404.blade.php
+++ resources/views/front/404.blade.php
@@ -3,16 +3,10 @@
 @section('content')
     <div class="consultationsSection about_anniiee_Section pad80">
         <div class="container">
-            <div class="">
-                <div class="text-center">
-
-                    <img src="{{ asset('front/images/404-error.png') }}" alt="" style="width: 500px;">
-                     <div class="session_intro">
-                        <p style="margin-top: 30px; color: orange;"><strong>Something is wrong</strong><br>
-The page you are looking for was moved, removed, renamed or might never existed.</p>
-                    </div>
-                </div>
-
+            <div class="error404">
+                <h1>Oops!</h1>
+                <img src="{{ asset('front/images/404Error.png') }}" alt="404Error">
+                <a href="{{route('front.home')}}" class="button"><span>Back to home</span></a>
             </div>
         </div>
     </div>
